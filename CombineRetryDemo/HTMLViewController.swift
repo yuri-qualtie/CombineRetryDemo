d481af1d421e80399c468d59608ed6977766a147
Yuri Goncharov <yurig@qualtie.com>
2021-03-11 15:26:03 +0300

    initial commit

---
+++ CombineRetryDemo/HTMLViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+import Combine
+
+class HTMLViewController: UIViewController {
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        URLSession.shared
+            .dataTaskPublisher(for: URL(string: "https://www.apple.com/")!)
+            .tryCatch { _ in
+                URLSession.shared
+                    .dataTaskPublisher(for: URL(string: "https://www.example.com/")!)
+            }
+            .retry(1)
+            .map {
+                String(decoding: $0.data, as: UTF8.self)
+            }
+            .sink(
+                receiveCompletion: { print("completed \($0)") },
+                receiveValue: { print("value \($0)") })
+            .store(in: &cancellables)
+    }
+    
+    deinit {
+        print("de inited")
+    }
+}
