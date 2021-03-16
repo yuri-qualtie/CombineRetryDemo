1abc35704192cd080e6f9e58e93269f43c3e816a
Paul Zabelin <paul@spartascience.com>
2021-03-16 21:48:09 +0300

    add playgrounds to illustrate just with and without print
    Co-authored-by: Yuri Goncharov <yurig@qualtie.com>
    Co-authored-by: Benjamín Summers <ben@spartascience.com>

---
+++ MyPlayground.playground/Pages/Just with print.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import Combine
+var cancellables = Set<AnyCancellable>()
+
+enum FruitError: Error {
+    case bad
+}
+var fruit = "apple"
+
+func getFruit() -> String {
+    fruit
+}
+
+
+Just(getFruit)
+    .print()
+    .map{$0()}
+    .flatMap { value -> AnyPublisher<String, FruitError> in
+        print("in flat map \(value)")
+        if value == "apple" {
+            fruit = "pineapple"
+            return Fail(error: FruitError.bad).eraseToAnyPublisher()
+        }
+        return Just("banana").setFailureType(to: FruitError.self).eraseToAnyPublisher()
+    }
+    .retry(1)
+    .sink(receiveCompletion: { print($0) }, receiveValue: {print($0)})
+    .store(in: &cancellables)
